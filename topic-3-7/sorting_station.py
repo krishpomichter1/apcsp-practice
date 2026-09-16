406477f2067376e5b2e4589d0bad16ee238a88ee
krishpomichter1 <krish.pomichter28@student.tamdistrict.org>
2026-09-16 22:01:48 +0000

    Work in smart

--- topic-3-7/sorting_station.py
+++ topic-3-7/sorting_station.py
@@ -1,14 +1,20 @@
 label = input()
-shape = label[0:3]
-color = label[4:6]
-size = int(label[7:9])
-mass = int(label[10:13])
+shape = label[0:4]
+color = label[4:7]
+size = int(label[7:10])
+mass = int(label[10:14])
 condition = label[14]
 
-if condition = "D" or size > 50 or mass > 2000:
+#Two equal signs
+destination = "unknown"
+
+if condition == "D" or size > 50 or mass > 2000:
     destination = "INSPECT"
 
-destination = "unknown"
+print(destination)
+print("shape: " + shape)
+print("size: " + str(size))
+print("mass: " + str(mass))
 
 
 #Use " " for ball if its a string
