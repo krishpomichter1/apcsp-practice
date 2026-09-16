3395244caa09abfddc7c291e1d60a3b2b3815f5a
krishpomichter1 <krish.pomichter28@student.tamdistrict.org>
2026-09-16 05:23:54 +0000

    more work at library

--- topic-3-7/sorting_station.py
+++ topic-3-7/sorting_station.py
@@ -1,9 +1,12 @@
 label = input()
 shape = label[0:3]
-color = [4:6]
-size = [7:9]
-mass = [10:13]
-condition = [14]
+color = label[4:6]
+size = int(label[7:9])
+mass = int(label[10:13])
+condition = label[14]
+
+if condition = "D" or size > 50 or mass > 2000:
+    destination = "INSPECT"
 
 destination = "unknown"
 
